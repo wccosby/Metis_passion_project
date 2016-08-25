96109eb4f414869dc827dcc68ecb2282bba29289
wccosby <cosby.william.c@gmail.com>
2016-08-25 13:27:56 -0700

    updating to n2nmodel implementation

--- models/base_model.py
+++ models/base_model.py
@@ -1,87 +1,112 @@
-import sys
+import os
 
 import numpy as np
 import tensorflow as tf
-from tqdm import tqdm
+import progressbar as pb
+
+from read_data import DataSet
 
 
 class BaseModel(object):
-    """ Code from mem2nn-tensorflow. """
-    def __init__(self, params, words):
+    def __init__(self, graph, params, name=None):
+        self.graph = graph
         self.params = params
-        self.words = words
         self.save_dir = params.save_dir
-
-        with tf.variable_scope('DMN'):
-            print("Building DMN...")
-            self.global_step = tf.Variable(0, name='global_step', trainable=False)
-            self.build()
+        self.name = name or self.__class__.__name__
+        self.initializer = tf.random_normal_initializer(params.init_mean, params.init_std)
+        with graph.as_default(), tf.variable_scope(self.name, initializer=self.initializer):
+            print("building %s tower ..." % self.name)
+            self.global_step = tf.get_variable('global_step', shape=[],
+                                               initializer=tf.constant_initializer(0), trainable=False)
+            self.learning_rate = tf.placeholder('float32', name='lr')
+            self.opt_op = None
+            self.correct_vec = None
+            self.total_loss = None
+            self.merged_summary = None
+            self._build_tower()
             self.saver = tf.train.Saver()
 
-    def build(self):
-        raise NotImplementedError()
+    def _build_tower(self):
+        raise Exception("Implement this!")
 
-    def get_feed_dict(self, batch, is_train):
-        raise NotImplementedError()
+    def _get_feed_dict(self, batch):
+        raise Exception("Implement this!")
 
-    def train_batch(self, sess, batch):
-        feed_dict = self.get_feed_dict(batch, is_train=True)
-        return sess.run([self.opt_op, self.global_step], feed_dict=feed_dict)
+    def train_batch(self, sess, learning_rate, batch):
+        feed_dict = self._get_feed_dict(batch)
+        feed_dict[self.learning_rate] = learning_rate
+        return sess.run([self.opt_op, self.merged_summary, self.global_step], feed_dict=feed_dict)
 
     def test_batch(self, sess, batch):
-        feed_dict = self.get_feed_dict(batch, is_train=False)
-        return sess.run([self.num_corrects, self.total_loss, self.global_step], feed_dict=feed_dict)
+        actual_batch_size = len(batch[0])
+        feed_dict = self._get_feed_dict(batch)
+        correct_vec, total_loss, summary_str, global_step = \
+            sess.run([self.correct_vec, self.total_loss, self.merged_summary, self.global_step], feed_dict=feed_dict)
+        num_corrects = np.sum(correct_vec[:actual_batch_size])
 
-    def train(self, sess, train_data, val_data):
+        return num_corrects, total_loss, summary_str, global_step
+
+    def train(self, sess, writer, train_data_set, val_data_set):
+        assert isinstance(train_data_set, DataSet)
+        assert isinstance(val_data_set, DataSet)
         params = self.params
+        learning_rate = params.init_lr
         num_epochs = params.num_epochs
-        num_batches = train_data.num_batches
+        num_batches = params.train_num_batches
+        anneal_period = params.anneal_period
+        anneal_ratio = params.anneal_ratio
 
-        print("Training %d epochs ..." % num_epochs)
-        for epoch_no in tqdm(range(num_epochs), desc='Epoch', maxinterval=86400, ncols=100):
-            for _ in range(num_batches):
-                batch = train_data.next_batch()
-                _, global_step = self.train_batch(sess, batch)
+        print("training %d epochs ..." % num_epochs)
+        for epoch_idx in range(num_epochs):
+            if epoch_idx > 0 and epoch_idx % anneal_period == 0:
+                learning_rate *= anneal_ratio
+            pbar = pb.ProgressBar(widgets=["epoch %d|" % (train_data_set.num_epochs_completed + 1),
+                                           pb.Percentage(), pb.Bar(), pb.ETA()], maxval=num_batches)
+            pbar.start()
+            for num_batches_completed in range(num_batches):
+                batch = train_data_set.get_next_labeled_batch()
+                _, summary_str, global_step = self.train_batch(sess, learning_rate, batch)
+                writer.add_summary(summary_str, global_step)
+                pbar.update(num_batches_completed)
+            pbar.finish()
+            train_data_set.complete_epoch()
 
-            train_data.reset()
+            if val_data_set and (epoch_idx + 1) % params.val_period == 0:
+                self.eval(sess, train_data_set, is_val=True)
+                self.eval(sess, val_data_set, is_val=True)
 
-            if (epoch_no + 1) % params.acc_period == 0:
-                print()  # Newline for TQDM
-                self.eval(sess, train_data, name='Training')
-
-            if val_data and (epoch_no + 1) % params.val_period == 0:
-                self.eval(sess, val_data, name='Validation')
-
-            if (epoch_no + 1) % params.save_period == 0:
+            if (epoch_idx + 1) % params.save_period == 0:
                 self.save(sess)
+        print("training done.")
 
-        print("Training completed.")
-
-    def eval(self, sess, data, name):
-        num_batches = data.num_batches
-        num_corrects = total = 0
+    def eval(self, sess, eval_data_set, is_val=False):
+        params = self.params
+        num_batches = params.val_num_batches if is_val else params.test_num_batches
+        num_corrects, total = 0, 0
+        string = "%s:N=%d|" % (eval_data_set.name, eval_data_set.batch_size * num_batches)
+        pbar = pb.ProgressBar(widgets=[string, pb.Percentage(), pb.Bar(), pb.ETA()], maxval=num_batches)
         losses = []
-        for _ in range(num_batches):
-            batch = data.next_batch()
-            cur_num_corrects, cur_loss, global_step = self.test_batch(sess, batch)
+        pbar.start()
+        for num_batches_completed in range(num_batches):
+            batch = eval_data_set.get_next_labeled_batch()
+            cur_num_corrects, cur_loss, _, global_step = self.test_batch(sess, batch)
             num_corrects += cur_num_corrects
             total += len(batch[0])
             losses.append(cur_loss)
-        data.reset()
+            pbar.update(num_batches_completed)
+        pbar.finish()
+        eval_data_set.reset()
         loss = np.mean(losses)
 
-        print("[%s] step %d: Accuracy = %.2f%% (%d / %d), Loss = %.4f" % \
-              (name, global_step, 100 * float(num_corrects) / total, num_corrects, total, loss))
-        return loss
+        print("at %d: acc = %.2f%% = %d / %d, loss = %.4f" %
+              (global_step, 100 * float(num_corrects)/total, num_corrects, total, loss))
 
     def save(self, sess):
-        print("Saving model to %s" % self.save_dir)
-        self.saver.save(sess, self.save_dir, self.global_step)
+        print("saving model ...")
+        save_path = os.path.join(self.save_dir, self.name)
+        self.saver.save(sess, save_path, self.global_step)
 
     def load(self, sess):
-        print("Loading model ...")
+        print("loading model ...")
         checkpoint = tf.train.get_checkpoint_state(self.save_dir)
-        if checkpoint is None:
-            print("Error: No saved model found. Please train first.")
-            sys.exit(0)
-self.saver.restore(sess, checkpoint.model_checkpoint_path)
+        self.saver.restore(sess, checkpoint.model_checkpoint_path)
